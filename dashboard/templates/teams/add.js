80382954f4ebc045010cf9a1bfbabd6807082895
Glitch (rafiki-dashboard) <none>
2022-04-05 19:05:34 +0000

    💵🙉 Checkpoint ./dashboard/templates/teams/add.js:35487188/431

---
+++ dashboard/templates/teams/add.js
@@ -0,0 +1,23 @@
+<div class="field">
+
+  <label class="label">Team Name</label>
+
+  <div class="control">
+
+    <input class="input" type="text" placeholder="e.g Sons Of Odin">
+
+  </div>
+
+</div>
+
+<div class="field">
+
+  <label class="label">Manager's Username</label>
+
+  <div class="control">
+
+    <input class="input" type="email" placeholder="">
+
+  </div>
+
+</div>
