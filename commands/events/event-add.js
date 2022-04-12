ed237238545aa18091ce3330fd3de88d80766cc4
Glitch (rafiki-dashboard) <none>
2022-04-12 15:20:06 +0000

    🎬👣 Checkpoint ./commands/events/event-add.js:35487188/3777

--- commands/events/event-add.js
+++ commands/events/event-add.js
@@ -1,70 +1,24 @@
 const { MessageEmbed } = require("discord.js");
-const event = require("../../models/event");
 
 module.exports = {
-  category: "Testing",
-  description: "Add event to the database", // Required for slash commands
-
-  slash: "both", // Create both a slash and legacy command
-  testOnly: true, // Only register a slash command for the testing guilds
-
+  slash: "both",
+  testOnly: true,
+  description: "Add an event to the database",
   minArgs: 3,
-  expectedArgs: "<eventname> <hostname> <hosttag>",
+  expectedArgs: "<event_name> <event_host> <event_tag>",
+  callback: ({ message, args }) => {
+    const embed = new MessageEmbed().setTitle("Example").setDescription("pong");
 
-  options: [
-    {
-      type: "SUBCOMMAND",
-      name: "add",
-      description: "Adds an event to the database",
-      options: [
-        {
-          name: "eventname",
-          description: "Name of your event",
-          required: true,
-          type: 3,
-        },
-        {
-          name: "hostname",
-          description: "Hosts Name",
-          required: true,
-          type: 3,
-        },
-        {
-          name: "hosttag",
-          description: "Hosts Discord @",
-          required: true,
-          type: 6,
-        },
-      ],
-    },
-  ],
+    const [event_name, event_host, event_tag] = args;
 
-  callback: ({ guild, message, interaction, args }) => {
-    const subCommand = interaction.options.getSubcommand();
-    const eventname = interaction.options.getString("eventname");
+    embed.addField("Name", event_name);
+    embed.addField("Host", event_host);
+    embed.addField("Orgniser", event_tag);
 
-    console.log(eventname);
-
-    const reply = "Pong!";
-
-    // message is provided only for a legacy command
     if (message) {
-      message.reply({
-        content: reply,
-      });
-      return;
+      message.reply("", { embed });
     }
 
-    // interaction is provided only for a slash command
-    interaction.reply({
-      content: reply,
-    });
-
-    // Alternatively we can just simply return our reply object
-    // OR just a string as the content.
-    // WOKCommands will handle the proper way to reply with it
-    return {
-      content: reply,
-    };
+    return embed;
   },
 };
