410472fd5945d0895bf8ef52216f56dd4a777839
Glitch (rafiki-dashboard) <none>
2022-04-12 10:57:17 +0000

    🏇🗽 Checkpoint ./index.js:35487188/9118

--- index.js
+++ index.js
@@ -12,81 +12,35 @@ const mongoose = require("mongoose");
 const GuildSettings = require("./models/settings");
 const Dashboard = require("./dashboard/dashboard");
 const { Client, Intents, Permissions } = require("discord.js");
+const WOKCommands = require("wokcommands");
+const path = require("path");
 
 // We instiate the client and connect to database.
-const client = new Client({
+const client = new DiscordJS.Client({
+  // These intents are recommended for the built in help menu
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
   ],
 });
-
 mongoose.connect(process.env.mongodbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// We listen for client's ready event.
 client.on("ready", async () => {
+  Dashboard(client);
+
   console.log("Fetching members...");
   for (const [id, guild] of client.guilds.cache) {
     await guild.members.fetch();
   }
   console.log("Fetched members.");
-
-  console.log(
-    `Bot is ready. (${client.guilds.cache.size} Guilds - ${client.channels.cache.size} Channels - ${client.users.cache.size} Users)`
-  );
-
-  client.user.setActivity("https://youtube.com/mattitsu", { type: "WATCHING" });
-
-  Dashboard(client);
-});
-
-// We listen for message events.
-client.on("messageCreate", async (message) => {
-  // Doing some basic command logic.
-  if (message.author.bot) return;
-  if (
-    !message.channel
-      .permissionsFor(message.guild.me)
-      .has(Permissions.FLAGS.SEND_MESSAGES)
-  ) {
-    return;
-  }
-
-  // Retriving the guild settings from database.
-  let storedSettings = await GuildSettings.findOne({
-    guildID: message.guild.id,
+  new WOKCommands(client, {
+    // The name of the local folder for your command files
+    commandsDir: path.join(__dirname, "commands"),
   });
-  if (!storedSettings) {
-    // If there are no settings stored for this guild, we create them and try to retrive them again.
-    const newSettings = new GuildSettings({
-      guildID: message.guild.id,
-    });
-    await newSettings.save().catch((e) => {
-      console.log(e);
-    });
-    storedSettings = await GuildSettings.findOne({ guildID: message.guild.id });
-  }
-  // If the message does not start with the prefix stored in database, we ignore the message.
-  if (message.content.indexOf(storedSettings.prefix) !== 0) return;
-
-  // We remove the prefix from the message and process the arguments.
-  const args = message.content
-    .slice(storedSettings.prefix.length)
-    .trim()
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // If command is ping we send a sample and then edit it with the latency.
-  if (command === "ping") {
-    const roundtripMessage = await message.channel.send({ content: "Pong!" });
-    return roundtripMessage.edit(
-      `*${roundtripMessage.createdTimestamp - message.createdTimestamp}ms*`
-    );
-  }
 });
 
 // Listening for error & warn events.
