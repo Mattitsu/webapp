59ddeb69028460a64fa0e3a2767a65956fddd8bc
Glitch (rafiki-dashboard) <none>
2022-04-13 13:28:41 +0000

    💼🛰 Checkpoint ./commands/events/event-list.js:35487188/5426

--- commands/events/event-list.js
+++ commands/events/event-list.js
@@ -3,12 +3,73 @@ const { MessageEmbed } = require("discord.js");
 const eventSchema = require("../../models/event");
 
 module.exports = {
-  slash: "both",
+  slash: true,
   testOnly: true,
   description: "List events in the database",
 
-  callback: async ({ args, interaction }) => {
+  guildOnly: true,
+  options: [
+    {
+      type: "SUB_COMMAND",
+      name: "add",
+      description: "Adds an event to the database",
+      options: [
+        {
+          name: "event name",
+          type: "STRING",
+          description: "Please enter an Event Name",
+          required: true,
+        },
+        {
+          name: "event date",
+          type: "STRING",
+          description: "Please enter an Event date",
+          required: true,
+        },
+        {
+          name: "event host",
+          type: "STRING",
+          description: "Please enter an Event Host Name",
+          required: true,
+        },
+        {
+          name: "event manager",
+          type: "USER",
+          description: "Please tag the event manager",
+          required: true,
+        },
+      ],
+      type: "SUB_COMMAND",
+      name: "remove",
+      description: "Remove an event from the database",
+      options: [
+        {
+          name: "event name",
+          type: "STRING",
+          description: "Please enter an Event Name",
+          required: true,
+        },
+      ],
+      type: "SUB_COMMAND",
+      name: "list",
+      description: "List event in the database",
+      options: [
+        {
+          name: "event name",
+          type: "STRING",
+          description: "Please enter an Event Name",
+          required: true,
+        },
+      ],
+    },
+  ],
+
+  callback: async ({ member, args, interaction }) => {
     //const [event_name, event_host, user, date, description] = args;
+    
+    const subCommand = interaction.options.getSubcommand()
+    
+    const user = interaction.options.getUser('user')
 
     const eventslist = eventSchema.find();
     console.log(eventslist);
